4ee3914ff509f8234c6254619793f8fa3c027c12
FranGlauber <fglauber912@gmail.com>
2026-08-26 09:41:21 -0300

    add main

--- main.py
+++ main.py
@@ -1,1 +1,22 @@
+from flask import Flask, render_template
 
+app = Flask(__name__)
+
+
+@app.route("/")
+def login():
+    return render_template("login.html")
+
+
+@app.route("/recepcao")
+def recepcao():
+    return render_template("dashboard_recepcao.html")
+
+
+@app.route("/administracao")
+def administracao():
+    return render_template("dashboard_admin.html")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
