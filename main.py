84b7adc59a788ffe8c70cdefb23499780000cdbf
anasarahlinhares-ux <anasarahlinhares@gmail.com>
2026-09-04 14:23:17 -0300

    adiciona telas e estilos do front-end

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -11,6 +11,24 @@ def login():
 @app.route("/recepcao")
 def recepcao():
     return render_template("dashboard_recepcao.html")
+
+
+@app.route("/pacientes")
+def pacientes():
+    return render_template("pacientes.html")
+
+
+@app.route("/solicitar_exames")
+def solicitar_exames():
+    return render_template("solicitar_exames.html")
+
+@app.route("/situacao_exames")
+def situacao_exames():
+    return render_template("situacao_exames.html")
+
+@app.route("/perfil")
+def perfil():
+    return render_template("perfil.html")
 
 
 @app.route("/administracao")
